04b576225ff3d024a75cc88fe78a27ab1a44d0e8
xjkg <gripenja@arcada.fi>
2022-11-27 00:10:21 +0200

    Main.js - ålder/pengar och time select

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 /* Site-wide JS i den här filen (t.ex. huvudmenyn) */
 
 console.log('main.js init'); // För att se att skriptet laddats in
+
+function ageButton(){
+    const age = document.querySelector("#age").value;
+    console.log(age);
+    if(age<18){
+        alert("Minimiåldern för att spela är 18")
+    }
+}
+
+function cashButton(){
+    const cash = document.querySelector("#cash").value;
+    console.log(cash)
+    cash.replace(",", ".")
+}
+
+function timeButton(){
+    const time = document.querySelector("#time").value;
+    console.log(`Time: ${time}`)
+}
