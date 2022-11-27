f4a6f00563e235010e445b3a7f3e24b7042c5b95
xjkg <gripenja@arcada.fi>
2022-11-27 15:58:42 +0200

    timer update

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,54 @@ function cashButton(){
 }
 
 function timeButton(){
-    const time = document.querySelector("#time").value;
-    console.log(`Time: ${time}`)
+    const d = new Date();
+    let addhour = 0;
+    let addmin = document.querySelector("#time").value;
+    let time = document.querySelector("#time").value;
+    if(time>=60)
+    {
+        addhour = time/60;
+        console.log(addhour)
+        addmin = 0;
+    }else{addhour = 0;}
+
+    let getmin = d.getMinutes()+Number(addmin)
+    let gethr = d.getHours()+Number(addhour)
+
+    if(getmin>59)
+    {
+        getmin = getmin-60;
+        if(getmin<10){getmin="0"+getmin}
+        gethr = gethr + 1
+        
+    }else if(getmin<10){if(getmin<10){getmin="0"+getmin}}
+    
+    
+    document.querySelector("#speltid").innerText = `Din speltid tar slut om ${time} minuter,
+     dvs kl. ${gethr}:${getmin}`
+     
 }
+
+function gameTimer(){
+        console.log("Game timer INIT")
+        const outputElem = document.querySelector("#speltimer");
+        let gameTimer = null
+
+        
+            if (gameTimer) clearInterval(gameTimer);
+
+            let minLeft = Number(document.querySelector("#time").value);
+
+            gameTimer = setInterval(function ()
+            {
+                outputElem.innerText = minLeft;
+                minLeft--;
+                console.log(minLeft)
+
+                if (minLeft <= 0) {
+                    clearInterval(gameTimer);
+                    outputElem.innerText = "Tiden är ute"
+                }
+            }(), 1000);
+        }
+        
