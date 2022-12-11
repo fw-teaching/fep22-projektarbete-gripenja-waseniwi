7cafe8d8ed12c8f489854a1b58c25747ef37de53
xjkg <gripenja@arcada.fi>
2022-12-11 21:25:53 +0200

    Nya sidor, localstorage cash, random memory

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,11 @@
 /* Site-wide JS i den här filen (t.ex. huvudmenyn) */
 
 console.log('main.js init'); // För att se att skriptet laddats in
-
 //Tracking
 
 
 function success(pos) {
     const location = pos.coords;
-
     console.log(`Platform: ${navigator.platform}
 Webbläsare: ${navigator.appName}
 Resolution: ${screen.availWidth} x ${screen.availHeight}
@@ -15,23 +13,23 @@ Fönsterstorlek: ${window.innerWidth} x ${window.innerHeight}
 Longitud: ${location.longitude}
 Latitud : ${location.latitude}`);
 
-}function error() {
+} function error() {
+    alert("No location access")
     console.log(`Platform: ${navigator.platform}
 Webbläsare: ${navigator.appName}
 Resolution: ${screen.availWidth} x ${screen.availHeight}
 Fönsterstorlek: ${window.innerWidth} x ${window.innerHeight}
 Longitud: Denied location access
 Latitud : Denied location access`)
-  }
-  navigator.geolocation.getCurrentPosition(success, error)
+}
+navigator.geolocation.getCurrentPosition(success, error)
 
 
 
 function gameCounter() {
     if (localStorage.getItem('visitCount')) {
         let currentCount = localStorage.getItem('visitCount');
-        document.querySelector("#counter").innerText = `
-    Du har besökt sidan ${localStorage.getItem('visitCount')} gånger.`;
+        document.querySelector("#counter").innerText = `Du har besökt sidan ${localStorage.getItem('visitCount')} gånger.`;
 
         currentCount++;
         localStorage.setItem('visitCount', currentCount);
@@ -41,37 +39,37 @@ function gameCounter() {
     }
 } gameCounter()
 
-function date(){
-const date = new Date();
-const hours = String(date.getHours()).padStart(2, '0');
-const minutes = String(date.getMinutes()).padStart(2, '0');
-const seconds = String(date.getSeconds()).padStart(2, '0');
-let d = new Date();
-d.setDate(d.getDate() + (((1 + 7 - d.getDay()) % 7) || 7));
-let distance = d - date;
-let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  let hours1 = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes1 = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds1 = Math.floor((distance % (1000 * 60)) / 1000);
-document.querySelector("#date").innerText = `Idag är det den ` + date.getDate() + `.` + (date.getMonth()+1) + `.` + date.getFullYear() + ` kl ` + hours + `:` + minutes + `:` + seconds;
-if(date.getDay() % 3){
-    alert("Casinot är stängt! Välkommen tillbaka om " + days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ");
-}
+function date() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2, '0');
+    const minutes = String(date.getMinutes()).padStart(2, '0');
+    const seconds = String(date.getSeconds()).padStart(2, '0');
+    let d = new Date();
+    d.setDate(d.getDate() + (((1 + 7 - d.getDay()) % 7) || 7));
+    let distance = d - date;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours1 = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes1 = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds1 = Math.floor((distance % (1000 * 60)) / 1000);
+    document.querySelector("#date").innerText = `Idag är det den ` + date.getDate() + `.` + (date.getMonth() + 1) + `.` + date.getFullYear() + ` kl ` + hours + `:` + minutes + `:` + seconds;
+    if (date.getDay() % 5) {
+        alert("Casinot är stängt! Välkommen tillbaka om " + days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ");
+    }
 }
 setInterval(date, 1000);
 
 
-function nameButton(){
-    let fname = 0;  
+function nameButton() {
+    let fname = 0;
     let lname = 0;
     fname = document.querySelector("#fname").value;
     lname = document.querySelector("#lname").value;
     const username = lname + fname.slice(0, 2);
-    if (fname == 0 || lname == 0){
+    if (fname == 0 || lname == 0) {
         alert("Fyll i både för- och efternamn!");
     }
-    if (fname != 0 && lname != 0){
+    if (fname != 0 && lname != 0) {
         document.querySelector("#användarnamn").innerText = `Välkommen ` + fname + `! Ditt användarnamn är ` + username + `.`
     }
 }
@@ -86,15 +84,21 @@ function ageButton() {
     }
 }
 
-let cash = Number(document.querySelector("#cash").value);
-function cashButton() { 
+
+let cash;
+cash = localStorage.getItem("cash", Number(cash)) || Number(document.querySelector("#cash").value);
+function cashButton() {
+    cash = localStorage.setItem("cash", Number(document.querySelector("#cash").value))
+    console.log(localStorage.getItem("cash", Number(cash)))
     cash = Number(document.querySelector("#cash").value);
-    if(cash<=0){alert("För lite pengar"); cash=0;}
-    else{
-    console.log(`Du valde ${cash} pengar`)
-    console.log(cash)
-    document.querySelector("#currentcash").innerHTML=`Du har ${cash} pengar`}
+    if (cash <= 0) { alert("För lite pengar"); cash = 0; }
+    else {
+        console.log(`Du valde ${cash} pengar`)
+        console.log(cash)
+        document.querySelector("#currentcash").innerHTML = `Du har ${cash} pengar`
+    }
 } document.querySelector("#btn-cash").addEventListener('click', cashButton);
+if (cash > 0) { document.querySelector("#currentcash").innerHTML = `Du har ${cash} pengar` }
 
 function timeButton() {
     const d = new Date();
@@ -119,7 +123,6 @@ function timeButton() {
     if (gethr > 23) { gethr = gethr - 24 }
 
     document.querySelector("#speltid").innerText = `Din speltid tar slut kl. ${gethr}:${getmin}`
-
 } document.querySelector("#btn-time").addEventListener('click', timeButton);
 
 
@@ -128,7 +131,9 @@ let timer = document.querySelector("#speltimer")
 
 function timerHandler() {
 
+
     if (gameTimer) clearInterval(gameTimer);
+
     let startingMinutes = document.querySelector("#time").value
     let time = startingMinutes * 60;
 
@@ -157,31 +162,7 @@ function timerHandler() {
 } document.querySelector("#btn-time").addEventListener('click', timerHandler);
 
 
-document.querySelectorAll('#memory > img').forEach((elem) => {
-    elem.addEventListener('mouseover', (evt) => {
-        evt.target.style.border = '5px solid red';
-    });
-    elem.addEventListener('mouseout', (evt) => {
-        evt.target.style.border = '0px';
-    });
-});
 
 
 
 
-document.querySelectorAll('#memory > img').forEach((elem) => {
-    const front = `./assets/images/card_${elem.id}.png`;
-    const back = `./assets/images/card_back.png`;
-    let state = true;
-    let img = elem;
-    elem.addEventListener('click', () => {
-        if (state) {
-            elem.setAttribute("src", front);
-            state = false;
-        }else{
-            elem.setAttribute("src", back);
-            state = true;
-        }
-    })
-}
-);
